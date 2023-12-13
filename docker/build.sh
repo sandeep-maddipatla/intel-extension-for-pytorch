567e7c447fb8f8ceec39bb434c38c210191a8b78
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2023-12-13 19:09:26 +0000

    Internal stable driver components in Dockerfile.prebuilt

--- docker/build.sh
+++ docker/build.sh
@@ -1,18 +1,16 @@
+
+
 #!/bin/bash
 
 if [[ ${IMAGE_NAME} != "" ]]; then
     docker build --build-arg http_proxy=$http_proxy \
                  --build-arg https_proxy=$https_proxy \
-                 --build-arg no_proxy=" " \
-                 --build-arg NO_PROXY=" " \
+                 --build-arg no_proxy=repositories.gfxs.intel.com \
+                 --build-arg NO_PROXY=$no_proxy \
                  --build-arg UNAME=ubuntu \
                  --build-arg GID=$(getent group render | sed -E 's,^render:[^:]*:([^:]*):.*$,\1,') \
                  --build-arg UBUNTU_VERSION=22.04 \
                  --build-arg PYTHON=python3.10 \
-                 --build-arg ICD_VER=23.30.26918.50-736~22.04 \
-                 --build-arg LEVEL_ZERO_GPU_VER=1.3.26918.50-736~22.04 \
-                 --build-arg LEVEL_ZERO_VER=1.13.1-719~22.04 \
-                 --build-arg LEVEL_ZERO_DEV_VER=1.13.1-719~22.04 \
                  --build-arg DPCPP_VER=2024.0.0-49819 \
                  --build-arg MKL_VER=2024.0.0-49656 \
                  --build-arg TORCH_VERSION=2.1.0a0+cxx11.abi \
